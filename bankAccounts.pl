91026b85c969ec9387b35c56643b1b8f9ae919b4
shaghayegh0 <119910382+shaghayegh0@users.noreply.github.com>
2024-11-01 12:06:22 -0400

    Add files via upload

---
+++ bankAccounts.pl
@@ -0,0 +1,73 @@
+
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+
+%%%%% SECTION: database
+%%%%% Put statements for account, created, lives, location and gender below
+
+
+
+
+%%%%% SECTION: lexicon
+%%%%% Put the rules/statements defining articles, adjectives, proper nouns, common nouns,
+%%%%% and prepositions in this section.
+%%%%% You should also put your lexicon helpers in this section
+%%%%% Your helpers should include at least the following:
+%%%%%       bank(X), person(X), man(X), woman(X), city(X), country(X)
+%%%%% You may introduce others as you see fit
+%%%%% DO NOT INCLUDE ANY statements for account, created, lives, location and gender 
+%%%%%     in this section
+
+
+
+%%%%% SECTION: parser
+%%%%% For testing your lexicon for question 3, we will use the default parser initially given to you.
+%%%%% ALL QUERIES IN QUESTION 3 and 4 SHOULD WORK WHEN USING THE DEFAULT PARSER
+%%%%% For testing your answers for question 5, we will use your parser below
+%%%%% You may include helper predicates for Question 5 here, but they
+%%%%% should not be needed for Question 3
+%%%%% DO NOT INCLUDE ANY statements for account, created, lives, location and gender 
+%%%%%     in this section
+
+what(Words, Ref) :- np(Words, Ref).
+
+/* Noun phrase can be a proper name or can start with an article */
+
+np([Name],Name) :- proper_noun(Name).
+np([Art|Rest], What) :- article(Art), np2(Rest, What).
+
+
+/* If a noun phrase starts with an article, then it must be followed
+   by another noun phrase that starts either with an adjective
+   or with a common noun. */
+
+np2([Adj|Rest],What) :- adjective(Adj,What), np2(Rest, What).
+np2([Noun|Rest], What) :- common_noun(Noun, What), mods(Rest,What).
+
+/* Modifier(s) provide an additional specific info about nouns.
+   Modifier can be a prepositional phrase followed by none, one or more
+   additional modifiers.  */
+
+mods([], _).
+mods(Words, What) :-
+	appendLists(Start, End, Words),
+	prepPhrase(Start, What),	mods(End, What).
+
+prepPhrase([Prep | Rest], What) :-
+	preposition(Prep, What, Ref), np(Rest, Ref).
+
+appendLists([], L, L).
+appendLists([H | L1], L2, [H | L3]) :-  appendLists(L1, L2, L3).
+
